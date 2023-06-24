f0b572dfe56a5b7025198afc4615d6724b2c9755
Bodya <bogdanzhovnir@gmail.com>
2023-06-24 12:47:49 +0200

    HWFINAL2.1

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -69,7 +69,10 @@ DATABASES = {
            'DB_ENGINE',
            default='django.db.backends.postgresql'
        ),
-       'NAME': os.getenv('POSTGRES_DB', default='postgres'),
+       'NAME': os.getenv(
+           'POSTGRES_DB',
+           default='postgres'
+       ),
        'USER': os.getenv('POSTGRES_USER', default='postgres'),
        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
        'HOST': os.getenv('DB_HOST', default='localhost'),
