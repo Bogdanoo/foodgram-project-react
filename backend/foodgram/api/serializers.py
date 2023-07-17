0883430c3ea8703ed105f95996c78da632105123
Bodya <bogdanzhovnir@gmail.com>
2023-07-17 22:58:53 +0200

    HWFINAL2.3

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -204,8 +204,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
 
         data['author'] = self.context['request'].user
 
-        if hasattr(data, 'get'):
-            return data
+        return data
 
     @staticmethod
     def create_ingredients(ingredients, recipe):
